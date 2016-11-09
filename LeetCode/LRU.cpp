3f03673328f563821baec70028c79da126fccc51
wuxing <wuxing@pset.suntec.net>
2016-11-09 10:26:29 +0800

    LRU

--- LeetCode/LRU.cpp
+++ LeetCode/LRU.cpp
@@ -2,27 +2,29 @@
 using namespace std;
 
 #define LRUCACHECOUNT 10
-Struct Node {
+struct DoubleNode {
     int key;
     string value;
-    Node* Prev;
-    Node* next;
-    Node():key(0),value(0),Prev(NULL),next(NULL){}
+    DoubleNode* prev;
+    DoubleNode* next;
+    DoubleNode():key(0),value(0),prev(NULL),next(NULL){}
 };
 
 
 
 class LRUCache {
+public:
     LRUCache();
     ~LRUCache();
+    void LRUCache_print();
 
 
 
 private:
     int Cache; 
     int Count;
-    Node* Head; // 把最近刚刚使用的节点或者刚建立的节点放在头节点；
-    Node* Tail; // Cache 已满的时候方便去掉尾节点（最近最少使用的那个节点）；
+    DoubleNode* Head; // 把最近刚刚使用的节点或者刚建立的节点放在头节点；
+    DoubleNode* Tail; // Cache 已满的时候方便去掉尾节点（最近最少使用的那个节点）;
     
 };
 
@@ -30,8 +32,36 @@ LRUCache::LRUCache():
     Cache(0),
     Count(LRUCACHECOUNT)
 {
-    
+    Head = new DoubleNode();
+    Tail = new DoubleNode();
+
+    Head->prev = NULL;
+    Head->next = Tail;
+    Tail->prev = Head;
+    Tail->next = NULL;
 }
+
+void LRUCache::LRUCache_print() 
+{
+    DoubleNode* temp_print = Head;
+
+    while(temp_print != Tail) 
+    {
+        cout << temp_print->value << " ";
+        temp_print = temp_print->next;
+    }
+    cout << endl;
+}
+
+
+int main() 
+{
+    LRUCache * cache = new LRUCache();
+    cache->LRUCache_print();
+}
+
+
+
 
 
 //疑问
