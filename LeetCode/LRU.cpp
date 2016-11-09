088bff0260bdb1793e0f79cec70e81cf924981e2
wuxing <wuxing@pset.suntec.net>
2016-11-09 09:57:42 +0800

    LRU

--- LeetCode/LRU.cpp
+++ LeetCode/LRU.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+#define LRUCACHECOUNT 10
 Struct Node {
     int key;
     string value;
@@ -19,20 +19,21 @@ class LRUCache {
 
 
 private:
-    Node* Head;
-    Node* Tail;
-    int Cache;
+    int Cache; 
     int Count;
+    Node* Head; // 把最近刚刚使用的节点或者刚建立的节点放在头节点；
+    Node* Tail; // Cache 已满的时候方便去掉尾节点（最近最少使用的那个节点）；
+    
 };
 
 LRUCache::LRUCache():
-Head(),
-Tail(),
-Cache(),
-Count()
-
+    Cache(0),
+    Count(LRUCACHECOUNT)
+{
+    
+}
 
 
 //疑问
 // 空的双链表怎么表示？
-//
+// 双连表和单链表一样也是由头指针唯一表示
