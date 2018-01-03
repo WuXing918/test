33b2066e1a0b30e4a5aa8f1ae5c1a100ef9b6f8a
wuxing <wu__xing@126.com>
2018-01-03 23:47:17 +0800

    heapSort

---
+++ sort/heapSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print(int a[], int length) {
+	for (int i = 0; i < length; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;	
+}
+
+HeapAdjust(list, i, list.length - 1);
+
+
+
+int main() {
+
+	int a[10] = {6,5,2,0,9,1,3,7,8,4};
+	print(a, 10);
+	// 建立大根堆
+	for (int i = 0; i < sizeof(a[])/2; i++) {
+		HeapAdjust
+	}
+		
+}
